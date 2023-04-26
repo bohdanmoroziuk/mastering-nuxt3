b935e31ebdac8b73740f4db7afe5fd25153dd594
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2023-04-26 14:40:50 +0300

    feat: show course completion percentage

--- stores/course-progress.ts
+++ stores/course-progress.ts
@@ -7,12 +7,60 @@ import { CourseProgress } from '~/types/course'
 export const useCourseProgressStore = defineStore('course-progress', () => {
   const progress = ref<CourseProgress>({})
 
+  const percentageCompleted = computed(() => {
+    const chapters = Object.values(progress.value).map(
+      (chapter) => {
+        const lessons = Object.values(chapter)
+        const completedLessons = lessons.filter(
+          lesson => lesson
+        )
+        return Number(
+          (completedLessons.length / lessons.length) * 100
+        ).toFixed(0)
+      },
+      []
+    )
+
+    const totalLessons = Object.values(
+      progress.value
+    ).reduce((number, chapter) => {
+      return number + Object.values(chapter).length
+    }, 0)
+
+    const totalCompletedLessons = Object.values(
+      progress.value
+    ).reduce((number, chapter) => {
+      return (
+        number +
+        Object.values(chapter).filter(lesson => lesson)
+          .length
+      )
+    }, 0)
+
+    const course = Number(
+      (totalCompletedLessons / totalLessons) * 100
+    ).toFixed(0)
+
+    return {
+      chapters,
+      course
+    }
+  })
+
   const initialized = ref(false)
 
   function initialize () {
     if (initialized.value) { return }
 
     initialized.value = true
+
+    const { data } = useFetch<CourseProgress>('/api/user/progress', {
+      headers: useRequestHeaders(['cookie']) as HeadersInit
+    })
+
+    if (data.value) {
+      progress.value = data.value
+    }
   }
 
   async function toggleComplete (chapter: string, lesson: string) {
@@ -44,6 +92,7 @@ export const useCourseProgressStore = defineStore('course-progress', () => {
 
   return {
     progress,
+    percentageCompleted,
     initialize,
     toggleComplete
   }

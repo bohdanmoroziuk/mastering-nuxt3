5a5aff26e8b386a43e65581e354fe2e34b53cdbf
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2023-04-27 14:35:08 +0300

    feat: add landing page

---
+++ components/Badge.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  color?: string;
+}
+
+withDefaults(
+  defineProps<Props>(),
+  {
+    color: 'bg-emerald-400'
+  }
+)
+</script>
+
+<template>
+  <span
+    class="-left-12 text-xl w-8 h-8 text-white rounded-full inline-flex absolute items-center justify-center mr-4"
+    :class="color"
+  >
+    <slot>&#10003;</slot>
+  </span>
+</template>
---
+++ components/Section.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+export default {
+  inheritAttrs: false
+}
+</script>
+
+<script setup lang="ts">
+interface Props {
+  title?: string;
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div
+    class="section bg-white p-24 flex flex-col items-center"
+  >
+    <div class="w-full max-w-4xl">
+      <h2
+        v-if="title"
+        class="text-4xl font-bold pb-8 text-center"
+      >
+        {{ title }}
+      </h2>
+      <div v-bind="$attrs">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.section:nth-of-type(even) {
+  @apply bg-blue-500 text-white;
+}
+</style>
